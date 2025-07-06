3a0bc148a14d8c6f09ce93b4f4fb77e47ee312ef
Winlypse <nts3@o2.pl>
2025-07-06 19:31:17 +0200

    code!

--- src/main/kotlin/winlyps/noChargedCreepers/NoChargedCreepers.kt
+++ src/main/kotlin/winlyps/noChargedCreepers/NoChargedCreepers.kt
@@ -1,14 +1,31 @@
 package winlyps.noChargedCreepers
 
+import org.bukkit.entity.Creeper
+import org.bukkit.entity.EntityType
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.CreeperPowerEvent
+import org.bukkit.event.entity.EntitySpawnEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-class NoChargedCreepers : JavaPlugin() {
+class NoChargedCreepers : JavaPlugin(), Listener {
 
     override fun onEnable() {
-        // Plugin startup logic
+        server.pluginManager.registerEvents(this, this)
     }
 
-    override fun onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    fun onCreeperPower(event: CreeperPowerEvent) {
+        event.isCancelled = true
+    }
+
+    @EventHandler
+    fun onEntitySpawn(event: EntitySpawnEvent) {
+        if (event.entityType != EntityType.CREEPER) return
+        val creeper = event.entity as Creeper
+        if (creeper.isPowered) {
+            creeper.isPowered = false
+        }
     }
 }
+
